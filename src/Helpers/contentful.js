fb426c7705c1f53b7c79355c2d4f2289b7acfb35
MmdMaTriS <mmdzeinolabedin@gmail.com>
2021-10-15 13:38:51 +0330

    Helpers Add,contentul added

---
+++ src/Helpers/contentful.js
@@ -0,0 +1,6 @@
+import * as SDK from "contentful";
+const contentful = SDK.createClient({
+  accessToken: "6yxt0fkxg5mo",
+  space: "XxUHNq7szydmbicd5QNfjDv0B3F0_X7y5BFAb4YmpaM",
+});
+export default contentful;
