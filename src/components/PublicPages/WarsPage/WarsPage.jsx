716b7935657424977f3ad8112d000b2729596d12
MmdMaTriS <mmdzeinolabedin@gmail.com>
2021-10-19 16:50:27 +0330

    Feat:WarsPage updated, rich text added, scss updated,contentfull updated.

--- src/components/PublicPages/WarsPage/WarsPage.jsx
+++ src/components/PublicPages/WarsPage/WarsPage.jsx
@@ -1,216 +1,65 @@
 import { Col, Row } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Classes from "./WarsPage.module.scss";
 import bgpic2 from "../../../images/contact-bg.jpg";
+import contentful from "../../../Helpers/contentful";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 const WarsPage = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    contentful
+      .getEntries({
+        content_type: "wars",
+      })
+      .then(setData);
+  }, []);
+  console.log(data);
   return (
     <>
       <section className={Classes.WarCardSEC}>
         <Row>
-          <Col md={8} sm={8}>
-            <div className={Classes.WarCard}>
-              <div className={Classes.BGCardPic}>
-                <img
-                  src={bgpic2}
-                  alt=""
-                  width="100%"
-                  style={{
-                    maxHeight: "220px",
-                    backgroundPosition: "center",
-                    backgroundSize: "cover",
-                  }}
-                />
-              </div>
-              <div className={Classes.history}>
-                <div className={Classes.historyMark}>
-                  <p>Namusan Salam</p>
+          {!data ? (
+            <h1>Loading...</h1>
+          ) : (
+            data.items.map((r) => (
+              <Col md={8} sm={8} key={r.fields.title}>
+                <div className={Classes.WarCard}>
+                  <div className={Classes.BGCardPic}>
+                    <img
+                      src={r.fields.image.fields.file.url}
+                      alt=""
+                      width="100%"
+                      style={{
+                        maxHeight: "220px",
+                        backgroundPosition: "center",
+                        backgroundSize: "cover",
+                      }}
+                    />
+                  </div>
+                  <div className={Classes.history}>
+                    <div className={Classes.historyMark}>
+                      <p>{r.fields.date}</p>
+                    </div>
+                  </div>
+                  <div className={Classes.CardTitle}>
+                    <h2>{r.fields.title}</h2>
+                  </div>
+                  <div className={Classes.DescriptionCard}>
+                    <p>
+                      <div
+                        dangerouslySetInnerHTML={{
+                          __html: documentToHtmlString(r.fields.desc),
+                        }}
+                      />
+                    </p>
+                  </div>
+                  <div className={Classes.CardBTN}>
+                    <button>...بیشتر بدانیم</button>
+                  </div>
                 </div>
-              </div>
-              <div className={Classes.CardTitle}>
-                <h2>نبرد هیتلر</h2>
-              </div>
-              <div className={Classes.DescriptionCard}>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Molestias quis ut quas pariatur voluptatum. Quisquam quod sint
-                  ...
-                </p>
-              </div>
-              <div className={Classes.CardBTN}>
-                <button>...بیشتر بدانیم</button>
-              </div>
-            </div>
-          </Col>
-          <Col md={8} sm={8}>
-            <div className={Classes.WarCard}>
-              <div className={Classes.BGCardPic}>
-                <img
-                  src={bgpic2}
-                  alt=""
-                  width="100%"
-                  style={{
-                    maxHeight: "220px",
-                    backgroundPosition: "center",
-                    backgroundSize: "cover",
-                  }}
-                />
-              </div>
-              <div className={Classes.history}>
-                <div className={Classes.historyMark}>
-                  <p>Namusan Salam</p>
-                </div>
-              </div>
-              <div className={Classes.CardTitle}>
-                <h2>نبرد هیتلر</h2>
-              </div>
-              <div className={Classes.DescriptionCard}>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Molestias quis ut quas pariatur voluptatum. Quisquam quod sint
-                  ...
-                </p>
-              </div>
-              <div className={Classes.CardBTN}>
-                <button>...بیشتر بدانیم</button>
-              </div>
-            </div>
-          </Col>
-          <Col md={8} sm={8}>
-            <div className={Classes.WarCard}>
-              <div className={Classes.BGCardPic}>
-                <img
-                  src={bgpic2}
-                  alt=""
-                  width="100%"
-                  style={{
-                    maxHeight: "220px",
-                    backgroundPosition: "center",
-                    backgroundSize: "cover",
-                  }}
-                />
-              </div>
-              <div className={Classes.history}>
-                <div className={Classes.historyMark}>
-                  <p>Namusan Salam</p>
-                </div>
-              </div>
-              <div className={Classes.CardTitle}>
-                <h2>نبرد هیتلر</h2>
-              </div>
-              <div className={Classes.DescriptionCard}>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Molestias quis ut quas pariatur voluptatum. Quisquam quod sint
-                  ...
-                </p>
-              </div>
-              <div className={Classes.CardBTN}>
-                <button>...بیشتر بدانیم</button>
-              </div>
-            </div>
-          </Col>
-          <Col md={8} sm={8}>
-            <div className={Classes.WarCard}>
-              <div className={Classes.BGCardPic}>
-                <img
-                  src={bgpic2}
-                  alt=""
-                  width="100%"
-                  style={{
-                    maxHeight: "220px",
-                    backgroundPosition: "center",
-                    backgroundSize: "cover",
-                  }}
-                />
-              </div>
-              <div className={Classes.history}>
-                <div className={Classes.historyMark}>
-                  <p>Namusan Salam</p>
-                </div>
-              </div>
-              <div className={Classes.CardTitle}>
-                <h2>نبرد هیتلر</h2>
-              </div>
-              <div className={Classes.DescriptionCard}>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Molestias quis ut quas pariatur voluptatum. Quisquam quod sint
-                  ...
-                </p>
-              </div>
-              <div className={Classes.CardBTN}>
-                <button>...بیشتر بدانیم</button>
-              </div>
-            </div>
-          </Col>
-          <Col md={8} sm={8}>
-            <div className={Classes.WarCard}>
-              <div className={Classes.BGCardPic}>
-                <img
-                  src={bgpic2}
-                  alt=""
-                  width="100%"
-                  style={{
-                    maxHeight: "220px",
-                    backgroundPosition: "center",
-                    backgroundSize: "cover",
-                  }}
-                />
-              </div>
-              <div className={Classes.history}>
-                <div className={Classes.historyMark}>
-                  <p>Namusan Salam</p>
-                </div>
-              </div>
-              <div className={Classes.CardTitle}>
-                <h2>نبرد هیتلر</h2>
-              </div>
-              <div className={Classes.DescriptionCard}>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Molestias quis ut quas pariatur voluptatum. Quisquam quod sint
-                  ...
-                </p>
-              </div>
-              <div className={Classes.CardBTN}>
-                <button>...بیشتر بدانیم</button>
-              </div>
-            </div>
-          </Col>
-          <Col md={8} sm={8}>
-            <div className={Classes.WarCard}>
-              <div className={Classes.BGCardPic}>
-                <img
-                  src={bgpic2}
-                  alt=""
-                  width="100%"
-                  style={{
-                    maxHeight: "220px",
-                    backgroundPosition: "center",
-                    backgroundSize: "cover",
-                  }}
-                />
-              </div>
-              <div className={Classes.history}>
-                <div className={Classes.historyMark}>
-                  <p>Namusan Salam</p>
-                </div>
-              </div>
-              <div className={Classes.CardTitle}>
-                <h2>نبرد هیتلر</h2>
-              </div>
-              <div className={Classes.DescriptionCard}>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Molestias quis ut quas pariatur voluptatum. Quisquam quod sint
-                  ...
-                </p>
-              </div>
-              <div className={Classes.CardBTN}>
-                <button>...بیشتر بدانیم</button>
-              </div>
-            </div>
-          </Col>
+              </Col>
+            ))
+          )}
         </Row>
       </section>
     </>
